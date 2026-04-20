7d6bbe9c2cc983f94cbcd3a0601c4b891245eb8a
Park SangHyeok <tkdgurehf00@gmail.com>
2026-04-20 21:38:38 +0900

    feat : 주소에 파일 이름, 확장자 표시

--- KraftonEngine/Source/Editor/UI/ContentBrowser/ContentBrowserElement.h
+++ KraftonEngine/Source/Editor/UI/ContentBrowser/ContentBrowserElement.h
@@ -8,10 +8,11 @@ class ContentBrowserElement
 {
 public:
 	virtual ~ContentBrowserElement() = default;
-	virtual void Render(ContentBrowserContext& Context) = 0;
+	virtual void Render(ContentBrowserContext& Context);
 	void SetIcon(ID3D11ShaderResourceView* InIcon) { Icon = InIcon; }
 	void SetContent(FContentItem InContent) { ContentItem = InContent; }
 
+	std::wstring GetFileName() {return ContentItem.Path.filename(); }
 protected:
 	FString EllipsisText(const FString& text, float maxWidth);
 
@@ -19,12 +20,6 @@ protected:
 	bool bIsSelected = false;
 	ID3D11ShaderResourceView* Icon = nullptr;
 	FContentItem ContentItem;
-};
-
-class DefaultElement final : public ContentBrowserElement
-{
-public:
-	void Render(ContentBrowserContext& Context) override;
 };
 
 class DirectoryElement final : public ContentBrowserElement
