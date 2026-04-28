30c7dc419dedc1f447901b9f94debba9540e4f87
LEE SangHoon <hoon15x@gmail.com>
2026-04-28 23:21:43 +0900

    fix: accurate light space AABB size
    원래 크기로 복구시킵니다. ShadowCasterDistance를 더는 반영하지 않습니다.

--- KraftonEngine/Source/Engine/Render/Scene/FScene.cpp
+++ KraftonEngine/Source/Engine/Render/Scene/FScene.cpp
@@ -393,12 +393,22 @@ void FScene::SubmitShadowFrustumDebug(UWorld* World, const FFrameContext& Frame)
 				);
 
 			FVector ShadowBoxCorners[8];
+			float MinZ = FLT_MAX;
+			float MaxZ = -FLT_MAX;
+
+			for (int32 i = 0; i < 8; ++i)
+			{
+				const FVector LS = DirectionalVP.View.TransformPositionWithW(CascadeCorners[i]);
+				MinZ = (std::min)(MinZ, LS.Z);
+				MaxZ = (std::max)(MaxZ, LS.Z);
+			}
+
 			FLightFrustumUtils::ComputeOrthoWorldCorners(
 				DirectionalVP.View,
 				DirectionalVP.OrthoWidth,
 				DirectionalVP.OrthoHeight,
-				DirectionalVP.NearZ,
-				DirectionalVP.FarZ,
+				MinZ,
+				MaxZ,
 				ShadowBoxCorners
 			);
 
